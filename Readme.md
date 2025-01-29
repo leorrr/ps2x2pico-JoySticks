83ba69eefdc54a8ffc85d149cdd1dba8591f5b94
Leorrr <leocroms@gmail.com>
2025-01-30 00:23:37 +0100

    Añadido soporte para GamePad USB imitacion  Nintendo Snes, mapeando los botones de la siguiente manera Para su uso en emulador ESPectrum
    	 - La cruceta como teclas de cursor
    	 - Boton A tecla ESCAPE
    	 - Boton B tecla TAB
    	 - Boton X Cursor Arriba
    	 - Boton Y tecla ESCAPE
    	 - Boton Select tecla F1
    	 - Boton Start tecla ENTER
    	 - Gatillo L tecla F2
    	 - Gatillo R tecla F5

--- Readme.md
+++ Readme.md
@@ -15,6 +15,18 @@ He diseñado una placa nueva inspirada en la placa con Rp2040 Zero de **Destroye
 Cuando la vi pense que lastima no aprovechar los pines que sobran.... y este es el resultado esta en fase beta, pero en princio es funcional, mapeadas las teclas de cursores para las direcciones y la tecla TAB para el disparo en el joy1, y las teclas 6,7,8,9,0 en el joy 2,pendiente dar soporte para gamepad de sega de tres y seis botones al igual que el proyecto de  **David Carrion** (  **https://github.com/dacarsoft/DB9_2_Keyboard** ) 
 o el fork del mismo de **Antonio Taimaron**, PowaJoy ( **https://github.com/hash6iron/PowaJoy**), el soporte para gamepad usb esta empezado, como puede verse en el codigo, pero no es apena usable, congela el emulador ESPectrum.
 
+Añadido soporte para GamePad USB imitacion  Nintendo Snes
+![GamePad](./images/SnesUsbControler.jpg)
+mapeando los botones de la siguiente manera, para su uso en emulador ESPectrum:
+- La cruceta como teclas de cursor
+- Boton A tecla ESCAPE
+- Boton B tecla TAB
+- Boton X Cursor Arriba
+- Boton Y tecla ESCAPE
+- Boton Select tecla F1
+- Boton Start tecla ENTER
+- Gatillo L tecla F2
+- Gatillo R tecla F5
 
 Animo a todo el que quiera unirse o desee continuarlo por su cuenta creando otro fork, lo haga libremente, yo por mi parte ire trabajando en el,  en la medida que mis conocimientos, me lo permitan... es lo que tiene programar de oido.
 En la carpeta PCB se pueden encontrar los pdf generados con eagle para quien quiera hacerse la placa, yo uso papel de transfer para circuitos, los imprimo con la impresora laser y seguidamente los plancho literalmente con la plancha de casa para transferirlo a la placa, depues agujeros, y a continuacion atacado del cobre con un baño de 3 partes de salfuman y 2 de agua oxigenada, y una vez eliminado el cobre sobrante limpieza con acetona para quitar el toner,   pintar con colofonia (resina de pino) para proteger las pista y facilitar su soldado y ya finalmente soldar los componente.
--- src/ps2gamepad.c
+++ src/ps2gamepad.c
@@ -11,88 +11,97 @@ bool joy1Up_state=0;
 bool joy1Down_state=0;
 bool joy1Left_state=0;
 bool joy1Right_state=0;
-bool joy1Fire_state=0;
+bool joy1A_state=0;
+bool joy1B_state=0;
+bool joy1X_state=0;
+bool joy1Y_state=0;
 bool joy1Select_state=0;
 bool joy1Start_state=0;
+bool joy1Ltrigger_state=0;
+bool joy1Rtrigger_state=0;
+
 
 bool joy2Up_state=0;
 bool joy2Down_state=0;
 bool joy2Left_state=0;
 bool joy2Right_state=0;
-bool joy2Fire_state=0;
+bool joy2A_state=0;
+bool joy2B_state=0;
+bool joy2X_state=0;
+bool joy2Y_state=0;
 bool joy2Select_state=0;
 bool joy2Start_state=0;
+bool joy2Ltrigger_state=0;
+bool joy2Rtrigger_state=0;
 
 
 void gamePad_usb_receive(u8 const* report) {
 //printf("none report:      %02x ,%02x ,%02x ,%02x ,%02x ,%02x ,%02x ,%02x  \r",report[0],report[1],report[2],report[3],report[4],report[5],report[6],report[7]);
-/*u8 is_modifiers=0; //por defecto a 0 a nosotros nos va bien eso se usa cuando hay el sift o el control apretados creo
-if (report[5]> 15){
-u8 key_TAB=0x2b; //Disparo
-//kb_send_key_scs2(key, is_key_pressed, is_ctrl);
-do{kb_send_key(key_TAB, 1, is_modifiers);sleep_ms(25);}
-while(report[5]== 15);
-  kb_send_key(key_TAB, 0, is_modifiers);
-}
 
-
-if (report[3]==0){
-u8 key_izq=0x50; //izquierda
-do{kb_send_key(key_izq, 1, is_modifiers);sleep_ms(25);}
-while (report[3]==127);
-  kb_send_key(key_izq, 0, is_modifiers);
-}
-
-if (report[3]==255){
-u8 key_der=0x4f; //Derecha
-do{kb_send_key(key_der, 1, is_modifiers);sleep_ms(25);}
-while (report[3]==127);  
-  kb_send_key(key_der, 0, is_modifiers);
-}
-
-
-if (report[4]==0){
-u8 key_arriba=0x52; //Arriba
-do{kb_send_key(key_arriba, true, is_modifiers);sleep_ms(25);}
-while (report[4]==127);
-  kb_send_key(key_arriba, false, is_modifiers);
-}
-
-if (report[4]==255){
-u8 key_abajo=0x51; // abajo
-do{kb_send_key(key_abajo, 1, is_modifiers);sleep_ms(25);}
-while (report[4]==127);
-  kb_send_key(key_abajo, 0, is_modifiers);
-}
  //printf("none report:      %d,%d,%d,%d,%d,%d,%d,       \r",report[0],report[1],report[2],report[3],report[4],report[5],report[6]);
-*/
-//disparo
-       if (report[5]>15 && joy1Fire_state==1){kb_send_key(0x2b, 1, 0);}          
-       else if (report[5]==15 && joy1Fire_state==1){kb_send_key(0x2b, 0, 0);joy1Fire_state=0;}
-       if (report[5]>15 && joy1Fire_state==0){kb_send_key(0x2b, 1, 0);joy1Fire_state=1;}
+
+//disparo boton B
+       if (report[5]==79 && joy1B_state==1){kb_send_key(0x2b, 1, 0);}          
+       else if (report[5]==15 && joy1B_state==1){kb_send_key(0x2b, 0, 0);joy1B_state=0;}
+       if (report[5]==79 && joy1B_state==0){kb_send_key(0x2b, 1, 0);joy1B_state=1;}
        
-      //izquierda
+//izquierda
        if (report[3]==0 && joy1Left_state==1){kb_send_key(0x50, 1, 0);} 
        else if (report[3]==127 && joy1Left_state==1){kb_send_key(0x50, 0, 0);joy1Left_state=0;}
        if (report[3]==0 && joy1Left_state==0){kb_send_key(0x50, 1, 0);joy1Left_state=1;}  
        
-        //Derecha
+//Derecha
        if (report[3]==255 && joy1Right_state==1){kb_send_key(0x4f, 1, 0);} 
        else if (report[3]==127 && joy1Right_state==1){kb_send_key(0x4f, 0, 0);joy1Right_state=0;}
        if (report[3]==255 && joy1Right_state==0){kb_send_key(0x4f, 1, 0);joy1Right_state=1;}  
        
 
-       //arriba
+//arriba
        if (report[4]==0 && joy1Up_state==1){kb_send_key(0x52, 1, 0);} 
        else if (report[4]==127 && joy1Up_state==1){kb_send_key(0x52, 0, 0);joy1Up_state=0;}
        if (report[4]==0 && joy1Up_state==0){kb_send_key(0x52, 1, 0);joy1Up_state=1;}  
         
        
-       //abajo
+//abajo
       if (report[4]==255 && joy1Down_state==1){kb_send_key(0x51, 1, 0);} 
       else if (report[4]==127 && joy1Down_state==1){kb_send_key(0x51, 0, 0);joy1Down_state=0;}
       if (report[4]==255 && joy1Down_state==0){kb_send_key(0x51, 1, 0);joy1Down_state=1;}     
      
+//ESCAPE boton A
+       if (report[5]==47 && joy1A_state==1){kb_send_key(0x29, 1, 0);}          
+       else if (report[5]==15 && joy1A_state==1){kb_send_key(0x29, 0, 0);joy1A_state=0;}
+       if (report[5]==47 && joy1A_state==0){kb_send_key(0x29, 1, 0);joy1A_state=1;}
+
+//arriba boton X
+       if (report[5]==31 && joy1X_state==1){kb_send_key(0x52, 1, 0);} 
+       else if (report[5]==15 && joy1X_state==1){kb_send_key(0x52, 0, 0);joy1X_state=0;}
+       if (report[5]==31 && joy1X_state==0){kb_send_key(0x52, 1, 0);joy1X_state=1;}  
+
+//ESCAPE boton Y
+       if (report[5]==143 && joy1Y_state==1){kb_send_key(0x29, 1, 0);}          
+       else if (report[5]==15 && joy1Y_state==1){kb_send_key(0x29, 0, 0);joy1Y_state=0;}
+       if (report[5]==143 && joy1Y_state==0){kb_send_key(0x29, 1, 0);joy1Y_state=1;}
+
+//F1 boton Select
+       if (report[6]==16 && joy1Select_state==1){kb_send_key(0x3A, 1, 0);} 
+       else if (report[6]==0 && joy1Select_state==1){kb_send_key(0x3A, 0, 0);joy1Select_state=0;}
+       if (report[6]==16 && joy1Select_state==0){kb_send_key(0x3A, 1, 0);joy1Select_state=1;} 
+
+//Enter boton Start
+       if (report[6]==32 && joy1Start_state==1){kb_send_key(0x28, 1, 0);} 
+       else if (report[6]==0 && joy1Start_state==1){kb_send_key(0x28, 0, 0);joy1Start_state=0;}
+       if (report[6]==32 && joy1Start_state==0){kb_send_key(0x28, 1, 0);joy1Start_state=1;}        
+
+//F2 boton Trigger L
+       if (report[6]==1 && joy1Ltrigger_state==1){kb_send_key(0x3B, 1, 0);} 
+       else if (report[6]==0 && joy1Ltrigger_state==1){kb_send_key(0x3B, 0, 0);joy1Ltrigger_state=0;}
+       if (report[6]==1 && joy1Ltrigger_state==0){kb_send_key(0x3B, 1, 0);joy1Ltrigger_state=1;} 
+
+ //F5 boton Trigger R
+       if (report[6]==2 && joy1Rtrigger_state==1){kb_send_key(0x3E, 1, 0);} 
+       else if (report[6]==0 && joy1Rtrigger_state==1){kb_send_key(0x3E, 0, 0);joy1Rtrigger_state=0;}
+       if (report[6]==2 && joy1Rtrigger_state==0){kb_send_key(0x3E, 1, 0);joy1Rtrigger_state=1;}       
+
      sleep_ms(80);  
 
 }

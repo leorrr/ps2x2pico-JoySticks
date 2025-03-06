539dc62dba25b1903eeb44f84cc64c3dd0fa1fb8
Leorrr <leocroms@gmail.com>
2025-03-06 21:37:27 +0100

    añadido soporte segundo gamepad usb

--- src/ps2gamepad.c
+++ src/ps2gamepad.c
@@ -99,42 +99,49 @@ if (gamePad1.left==1 && gamePad1.lastLeft==0){Esp_code_send(ESP_JOY1LEFT,1);game
 //derecha
 if (gamePad1.right==1 && gamePad1.lastRight==1){Esp_code_send(ESP_JOY1RIGHT,1);} //si esta pulsado
 else if (gamePad1.right==0 && gamePad1.lastRight==1){Esp_code_send(ESP_JOY1RIGHT,0);gamePad1.lastRight=0;}//si hemos soltado
-if (gamePad1.right==1 && gamePad1.lastRight==0){Esp_code_send(ESP_JOY1RIGHT,1);gamePad1.lastRight;}//Lo acabamos de pulsar
+if (gamePad1.right==1 && gamePad1.lastRight==0){Esp_code_send(ESP_JOY1RIGHT,1);gamePad1.lastRight=1;}//Lo acabamos de pulsar
 
 //Boton A
 if (gamePad1.buttonA==1 && gamePad1.lastButtonA==1){Esp_code_send(ESP_JOY1A,1);} //si esta pulsado
 else if (gamePad1.buttonA==0 && gamePad1.lastButtonA==1){Esp_code_send(ESP_JOY1A,0);gamePad1.lastButtonA=0;}//si hemos soltado
-if (gamePad1.buttonA==1 && gamePad1.lastButtonA==0){Esp_code_send(ESP_JOY1A,1);gamePad1.lastButtonA;}//Lo acabamos de pulsar
+if (gamePad1.buttonA==1 && gamePad1.lastButtonA==0){Esp_code_send(ESP_JOY1A,1);gamePad1.lastButtonA=1;}//Lo acabamos de pulsar
 
 //Boton B
 if (gamePad1.buttonB==1 && gamePad1.lastButtonB==1){Esp_code_send(ESP_JOY1B,1);} //si esta pulsado
 else if (gamePad1.buttonA==0 && gamePad1.lastButtonB==1){Esp_code_send(ESP_JOY1B,0);gamePad1.lastButtonB=0;}//si hemos soltado
-if (gamePad1.buttonB==1 && gamePad1.lastButtonB==0){Esp_code_send(ESP_JOY1B,1);gamePad1.lastButtonB;}//Lo acabamos de pulsar
+if (gamePad1.buttonB==1 && gamePad1.lastButtonB==0){Esp_code_send(ESP_JOY1B,1);gamePad1.lastButtonB=1;}//Lo acabamos de pulsar
 
 //Boton X
 if (gamePad1.buttonX==1 && gamePad1.lastButtonX==1){Esp_code_send(ESP_JOY1X,1);} //si esta pulsado
 else if (gamePad1.buttonX==0 && gamePad1.lastButtonX==1){Esp_code_send(ESP_JOY1X,0);gamePad1.lastButtonX=0;}//si hemos soltado
-if (gamePad1.buttonX==1 && gamePad1.lastButtonX==0){Esp_code_send(ESP_JOY1X,1);gamePad1.lastButtonX;}//Lo acabamos de pulsar
+if (gamePad1.buttonX==1 && gamePad1.lastButtonX==0){Esp_code_send(ESP_JOY1X,1);gamePad1.lastButtonX=1;}//Lo acabamos de pulsar
+
+//Boton Y
+if (gamePad1.ButtonY==1 && gamePad1.lastButtonY==1){Esp_code_send(ESP_JOY1Y,1);} //si esta pulsado
+else if (gamePad1.ButtonY==0 && gamePad1.lastButtonY==1){Esp_code_send(ESP_JOY1Y,0);gamePad1.lastButtonY=0;}//si hemos soltado
+if (gamePad1.ButtonY==1 && gamePad1.lastButtonY==0){Esp_code_send(ESP_JOY1Y,1);gamePad1.lastButtonY=1;}//Lo acabamos de pulsar
+
+
 
 //Select
 if (gamePad1.select==1 && gamePad1.lastSelect==1){Esp_code_send(ESP_JOY1MODE,1);} //si esta pulsado
 else if (gamePad1.select==0 && gamePad1.lastSelect==1){Esp_code_send(ESP_JOY1MODE,0);gamePad1.lastSelect=0;}//si hemos soltado
-if (gamePad1.select==1 && gamePad1.lastSelect==0){Esp_code_send(ESP_JOY1MODE,1);gamePad1.lastSelect;}//Lo acabamos de pulsar
+if (gamePad1.select==1 && gamePad1.lastSelect==0){Esp_code_send(ESP_JOY1MODE,1);gamePad1.lastSelect=1;}//Lo acabamos de pulsar
 
 //Start
 if (gamePad1.start==1 && gamePad1.lastStart==1){Esp_code_send(ESP_JOY1START,1);} //si esta pulsado
 else if (gamePad1.start==0 && gamePad1.lastStart==1){Esp_code_send(ESP_JOY1START,0);gamePad1.lastStart=0;}//si hemos soltado
-if (gamePad1.start==1 && gamePad1.lastStart==0){Esp_code_send(ESP_JOY1START,1);gamePad1.lastStart;}//Lo acabamos de pulsar
+if (gamePad1.start==1 && gamePad1.lastStart==0){Esp_code_send(ESP_JOY1START,1);gamePad1.lastStart=1;}//Lo acabamos de pulsar
 
 //LTrigger
 if (gamePad1.Ltrigger==1 && gamePad1.lastLtrigger==1){Esp_code_send(ESP_JOY1C,1);} //si esta pulsado
-else if (gamePad1.Ltrigger==0 && gamePad1.lastLtrigger==1){Esp_code_send(ESP_JOY1START,0);gamePad1.lastLtrigger=0;}//si hemos soltado
-if (gamePad1.Ltrigger==1 && gamePad1.lastLtrigger==0){Esp_code_send(ESP_JOY1START,1);gamePad1.lastLtrigger;}//Lo acabamos de pulsar
+else if (gamePad1.Ltrigger==0 && gamePad1.lastLtrigger==1){Esp_code_send(ESP_JOY1C,0);gamePad1.lastLtrigger=0;}//si hemos soltado
+if (gamePad1.Ltrigger==1 && gamePad1.lastLtrigger==0){Esp_code_send(ESP_JOY1C,1);gamePad1.lastLtrigger=1;}//Lo acabamos de pulsar
 
 //RTrigger
 if (gamePad1.Rtrigger==1 && gamePad1.lastRtrigger==1){Esp_code_send(ESP_JOY1Z,1);} //si esta pulsado
 else if (gamePad1.Rtrigger==0 && gamePad1.lastRtrigger==1){Esp_code_send(ESP_JOY1Z,0);gamePad1.lastRtrigger=0;}//si hemos soltado
-if (gamePad1.Rtrigger==1 && gamePad1.lastRtrigger==0){Esp_code_send(ESP_JOY1Z,1);gamePad1.lastRtrigger;}//Lo acabamos de pulsar
+if (gamePad1.Rtrigger==1 && gamePad1.lastRtrigger==0){Esp_code_send(ESP_JOY1Z,1);gamePad1.lastRtrigger=1;}//Lo acabamos de pulsar
 
 }
 
@@ -142,59 +149,65 @@ void Gamepad2Process(u8 const* report)
 {
        readGamepad2(&gamePad2, report);
 //arriba
-if (gamePad2.up==1 && gamePad2.lastUp==1){Esp_code_send(ESP_JOY1UP,1);} //si esta pulsado
-else if (gamePad2.up==0 && gamePad2.lastUp==1){Esp_code_send(ESP_JOY1UP,0);gamePad2.lastUp=0;}//si hemos soltado
-if (gamePad2.up==1 && gamePad2.lastUp==0){Esp_code_send(ESP_JOY1UP,1);gamePad2.lastUp=1;}//Lo acabamos de pulsar
+if (gamePad2.up==1 && gamePad2.lastUp==1){Esp_code_send(ESP_JOY2UP,1);} //si esta pulsado
+else if (gamePad2.up==0 && gamePad2.lastUp==1){Esp_code_send(ESP_JOY2UP,0);gamePad2.lastUp=0;}//si hemos soltado
+if (gamePad2.up==1 && gamePad2.lastUp==0){Esp_code_send(ESP_JOY2UP,1);gamePad2.lastUp=1;}//Lo acabamos de pulsar
 
 //Abajo
-if (gamePad2.down==1 && gamePad2.lastDown==1){Esp_code_send(ESP_JOY1DOWN,1);} //si esta pulsado
-else if (gamePad2.down==0 && gamePad2.lastDown==1){Esp_code_send(ESP_JOY1DOWN,0);gamePad2.lastDown=0;}//si hemos soltado
-if (gamePad2.down==1 && gamePad2.lastDown==0){Esp_code_send(ESP_JOY1DOWN,1);gamePad2.lastDown=1;}//Lo acabamos de pulsar
+if (gamePad2.down==1 && gamePad2.lastDown==1){Esp_code_send(ESP_JOY2DOWN,1);} //si esta pulsado
+else if (gamePad2.down==0 && gamePad2.lastDown==1){Esp_code_send(ESP_JOY2DOWN,0);gamePad2.lastDown=0;}//si hemos soltado
+if (gamePad2.down==1 && gamePad2.lastDown==0){Esp_code_send(ESP_JOY2DOWN,1);gamePad2.lastDown=1;}//Lo acabamos de pulsar
 
 //izquierda
-if (gamePad2.left==1 && gamePad2.lastLeft==1){Esp_code_send(ESP_JOY1LEFT,1);} //si esta pulsado
-else if (gamePad2.left==0 && gamePad2.lastLeft==1){Esp_code_send(ESP_JOY1LEFT,0);gamePad2.lastLeft=0;}//si hemos soltado
-if (gamePad2.left==1 && gamePad2.lastLeft==0){Esp_code_send(ESP_JOY1LEFT,1);gamePad2.lastLeft=1;}//Lo acabamos de pulsar
+if (gamePad2.left==1 && gamePad2.lastLeft==1){Esp_code_send(ESP_JOY2LEFT,1);} //si esta pulsado
+else if (gamePad2.left==0 && gamePad2.lastLeft==1){Esp_code_send(ESP_JOY2LEFT,0);gamePad2.lastLeft=0;}//si hemos soltado
+if (gamePad2.left==1 && gamePad2.lastLeft==0){Esp_code_send(ESP_JOY2LEFT,1);gamePad2.lastLeft=1;}//Lo acabamos de pulsar
 
 //derecha
-if (gamePad2.right==1 && gamePad2.lastRight==1){Esp_code_send(ESP_JOY1RIGHT,1);} //si esta pulsado
-else if (gamePad2.right==0 && gamePad2.lastRight==1){Esp_code_send(ESP_JOY1RIGHT,0);gamePad2.lastRight=0;}//si hemos soltado
-if (gamePad2.right==1 && gamePad2.lastRight==0){Esp_code_send(ESP_JOY1RIGHT,1);gamePad2.lastRight;}//Lo acabamos de pulsar
+if (gamePad2.right==1 && gamePad2.lastRight==1){Esp_code_send(ESP_JOY2RIGHT,1);} //si esta pulsado
+else if (gamePad2.right==0 && gamePad2.lastRight==1){Esp_code_send(ESP_JOY2RIGHT,0);gamePad2.lastRight=0;}//si hemos soltado
+if (gamePad2.right==1 && gamePad2.lastRight==0){Esp_code_send(ESP_JOY2RIGHT,1);gamePad2.lastRight=1;}//Lo acabamos de pulsar
 
 //Boton A
-if (gamePad2.buttonA==1 && gamePad2.lastButtonA==1){Esp_code_send(ESP_JOY1A,1);} //si esta pulsado
-else if (gamePad2.buttonA==0 && gamePad2.lastButtonA==1){Esp_code_send(ESP_JOY1A,0);gamePad2.lastButtonA=0;}//si hemos soltado
-if (gamePad2.buttonA==1 && gamePad2.lastButtonA==0){Esp_code_send(ESP_JOY1A,1);gamePad2.lastButtonA;}//Lo acabamos de pulsar
+if (gamePad2.buttonA==1 && gamePad2.lastButtonA==1){Esp_code_send(ESP_JOY2A,1);} //si esta pulsado
+else if (gamePad2.buttonA==0 && gamePad2.lastButtonA==1){Esp_code_send(ESP_JOY2A,0);gamePad2.lastButtonA=0;}//si hemos soltado
+if (gamePad2.buttonA==1 && gamePad2.lastButtonA==0){Esp_code_send(ESP_JOY2A,1);gamePad2.lastButtonA=1;}//Lo acabamos de pulsar
 
 //Boton B
-if (gamePad2.buttonB==1 && gamePad2.lastButtonB==1){Esp_code_send(ESP_JOY1B,1);} //si esta pulsado
-else if (gamePad2.buttonA==0 && gamePad2.lastButtonB==1){Esp_code_send(ESP_JOY1B,0);gamePad2.lastButtonB=0;}//si hemos soltado
-if (gamePad2.buttonB==1 && gamePad2.lastButtonB==0){Esp_code_send(ESP_JOY1B,1);gamePad2.lastButtonB;}//Lo acabamos de pulsar
+if (gamePad2.buttonB==1 && gamePad2.lastButtonB==1){Esp_code_send(ESP_JOY2B,1);} //si esta pulsado
+else if (gamePad2.buttonA==0 && gamePad2.lastButtonB==1){Esp_code_send(ESP_JOY2B,0);gamePad2.lastButtonB=0;}//si hemos soltado
+if (gamePad2.buttonB==1 && gamePad2.lastButtonB==0){Esp_code_send(ESP_JOY2B,1);gamePad2.lastButtonB=1;}//Lo acabamos de pulsar
 
 //Boton X
-if (gamePad2.buttonX==1 && gamePad2.lastButtonX==1){Esp_code_send(ESP_JOY1X,1);} //si esta pulsado
-else if (gamePad2.buttonX==0 && gamePad2.lastButtonX==1){Esp_code_send(ESP_JOY1X,0);gamePad2.lastButtonX=0;}//si hemos soltado
-if (gamePad2.buttonX==1 && gamePad2.lastButtonX==0){Esp_code_send(ESP_JOY1X,1);gamePad2.lastButtonX;}//Lo acabamos de pulsar
+if (gamePad2.buttonX==1 && gamePad2.lastButtonX==1){Esp_code_send(ESP_JOY2X,1);} //si esta pulsado
+else if (gamePad2.buttonX==0 && gamePad2.lastButtonX==1){Esp_code_send(ESP_JOY2X,0);gamePad2.lastButtonX=0;}//si hemos soltado
+if (gamePad2.buttonX==1 && gamePad2.lastButtonX==0){Esp_code_send(ESP_JOY2X,1);gamePad2.lastButtonX=1;}//Lo acabamos de pulsar
+
+//Boton Y
+if (gamePad2.ButtonY==1 && gamePad2.lastButtonY==1){Esp_code_send(ESP_JOY2Y,1);} //si esta pulsado
+else if (gamePad2.ButtonY==0 && gamePad2.lastButtonY==1){Esp_code_send(ESP_JOY2Y,0);gamePad2.lastButtonY=0;}//si hemos soltado
+if (gamePad2.ButtonY==1 && gamePad2.lastButtonY==0){Esp_code_send(ESP_JOY2Y,1);gamePad2.lastButtonY=1;}//Lo acabamos de pulsar
+
 
 //Select
-if (gamePad2.select==1 && gamePad2.lastSelect==1){Esp_code_send(ESP_JOY1MODE,1);} //si esta pulsado
-else if (gamePad2.select==0 && gamePad2.lastSelect==1){Esp_code_send(ESP_JOY1MODE,0);gamePad2.lastSelect=0;}//si hemos soltado
-if (gamePad2.select==1 && gamePad2.lastSelect==0){Esp_code_send(ESP_JOY1MODE,1);gamePad2.lastSelect;}//Lo acabamos de pulsar
+if (gamePad2.select==1 && gamePad2.lastSelect==1){Esp_code_send(ESP_JOY2MODE,1);} //si esta pulsado
+else if (gamePad2.select==0 && gamePad2.lastSelect==1){Esp_code_send(ESP_JOY2MODE,0);gamePad2.lastSelect=0;}//si hemos soltado
+if (gamePad2.select==1 && gamePad2.lastSelect==0){Esp_code_send(ESP_JOY2MODE,1);gamePad2.lastSelect=1;}//Lo acabamos de pulsar
 
 //Start
-if (gamePad2.start==1 && gamePad2.lastStart==1){Esp_code_send(ESP_JOY1START,1);} //si esta pulsado
-else if (gamePad2.start==0 && gamePad2.lastStart==1){Esp_code_send(ESP_JOY1START,0);gamePad2.lastStart=0;}//si hemos soltado
-if (gamePad2.start==1 && gamePad2.lastStart==0){Esp_code_send(ESP_JOY1START,1);gamePad2.lastStart;}//Lo acabamos de pulsar
+if (gamePad2.start==1 && gamePad2.lastStart==1){Esp_code_send(ESP_JOY2START,1);} //si esta pulsado
+else if (gamePad2.start==0 && gamePad2.lastStart==1){Esp_code_send(ESP_JOY2START,0);gamePad2.lastStart=0;}//si hemos soltado
+if (gamePad2.start==1 && gamePad2.lastStart==0){Esp_code_send(ESP_JOY2START,1);gamePad2.lastStart=1;}//Lo acabamos de pulsar
 
 //LTrigger
-if (gamePad2.Ltrigger==1 && gamePad2.lastLtrigger==1){Esp_code_send(ESP_JOY1C,1);} //si esta pulsado
-else if (gamePad2.Ltrigger==0 && gamePad2.lastLtrigger==1){Esp_code_send(ESP_JOY1START,0);gamePad2.lastLtrigger=0;}//si hemos soltado
-if (gamePad2.Ltrigger==1 && gamePad2.lastLtrigger==0){Esp_code_send(ESP_JOY1START,1);gamePad2.lastLtrigger;}//Lo acabamos de pulsar
+if (gamePad2.Ltrigger==1 && gamePad2.lastLtrigger==1){Esp_code_send(ESP_JOY2C,1);} //si esta pulsado
+else if (gamePad2.Ltrigger==0 && gamePad2.lastLtrigger==1){Esp_code_send(ESP_JOY2C,0);gamePad2.lastLtrigger=0;}//si hemos soltado
+if (gamePad2.Ltrigger==1 && gamePad2.lastLtrigger==0){Esp_code_send(ESP_JOY2C,1);gamePad2.lastLtrigger=1;}//Lo acabamos de pulsar
 
 //RTrigger
-if (gamePad2.Rtrigger==1 && gamePad2.lastRtrigger==1){Esp_code_send(ESP_JOY1Z,1);} //si esta pulsado
-else if (gamePad2.Rtrigger==0 && gamePad2.lastRtrigger==1){Esp_code_send(ESP_JOY1Z,0);gamePad2.lastRtrigger=0;}//si hemos soltado
-if (gamePad2.Rtrigger==1 && gamePad2.lastRtrigger==0){Esp_code_send(ESP_JOY1Z,1);gamePad2.lastRtrigger;}//Lo acabamos de pulsar
+if (gamePad2.Rtrigger==1 && gamePad2.lastRtrigger==1){Esp_code_send(ESP_JOY2Z,1);} //si esta pulsado
+else if (gamePad2.Rtrigger==0 && gamePad2.lastRtrigger==1){Esp_code_send(ESP_JOY2Z,0);gamePad2.lastRtrigger=0;}//si hemos soltado
+if (gamePad2.Rtrigger==1 && gamePad2.lastRtrigger==0){Esp_code_send(ESP_JOY2Z,1);gamePad2.lastRtrigger=1;}//Lo acabamos de pulsar
 
 }
 
